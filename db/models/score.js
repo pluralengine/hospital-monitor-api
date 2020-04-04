3cbb67b3ddb91a013ca52bb85021dca13da12bbe
Ivan Ortega Alba <ivanortegaalba@gmail.com>
2020-04-04 18:13:00 +0200

    chore: remove unused code

--- db/models/score.js
+++ db/models/score.js
@@ -3,8 +3,5 @@ module.exports = (sequelize, DataTypes) => {
   const Score = sequelize.define('Score', {
     rate: DataTypes.INTEGER
   }, {});
-  Score.associate = function(models) {
-    // associations can be defined here
-  };
   return Score;
 };
