4871be4d254239b14ae825b5db3e2734b22c61f3
Ivan Ortega <iortega@newrelic.com>
2020-04-04 21:58:54 +0200

    fix: db configuration is not correclty set

--- db/config/index.js
+++ db/config/index.js
@@ -3,27 +3,26 @@ var envDbConfig = process.env.DATABASE_URL
   ? parseDbUrl(process.env.DATABASE_URL)
   : {};
 
-console.log(envDbConfig);
 module.exports = {
   development: {
-    username: envDbConfig.dialect || 'medrank',
+    username: envDbConfig.user || 'medrank',
     password: envDbConfig.password || null,
     database: envDbConfig.database || 'hospital-monitor',
     host: envDbConfig.host || 'localhost',
-    dialect: envDbConfig.dialect || 'postgres',
+    dialect: envDbConfig.driver || 'postgres',
   },
   test: {
     username: envDbConfig.user || process.env.DB_USER,
     password: envDbConfig.password || process.env.DB_PWD,
     database: envDbConfig.database || process.env.DB_NAME,
     host: envDbConfig.host || process.env.HOST,
-    dialect: envDbConfig.dialect || 'postgres',
+    dialect: envDbConfig.driver || 'postgres',
   },
   production: {
     username: envDbConfig.user || process.env.DB_USER,
     password: envDbConfig.password || process.env.DB_PWD,
     database: envDbConfig.database || process.env.DB_NAME,
     host: envDbConfig.host || process.env.HOST,
-    dialect: envDbConfig.dialect || 'postgres',
+    dialect: envDbConfig.driver || 'postgres',
   },
 };
