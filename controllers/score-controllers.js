0cd109d75666d853bb1e5e3c7cb33ffdcb88bbc0
martacolombas <martacolombas@gmail.com>
2020-04-05 18:42:51 +0200

    fix: changed naming of some variables for better explanation and add throw error to keep propagating it until the right promise catches the error

--- controllers/score-controllers.js
+++ controllers/score-controllers.js
@@ -1,7 +1,7 @@
 const { Hospital, Score } = require('../db/models');
 const Sequelize = require('sequelize');
 
-exports.sendHospitalScore = function(req, res) {
+exports.sendHospitalScore = function (req, res) {
   const hospitalId = req.query.hospitalId;
   const score = req.query.score;
   const userid = req.query.userId;
@@ -11,23 +11,24 @@ exports.sendHospitalScore = function(req, res) {
     UserId: userid,
     HospitalId: hospitalId,
   }).then(() => {
-      const Op = Sequelize.Op;
-      Score.findAll({
-        where: {
-          HospitalId: hospitalId,
-          createdAt: {
-            [Op.lt]: new Date(),
-            // last 15 min records
-            [Op.gt]: new Date(new Date() - 15 * 60 * 1000),
-          },
+    const Op = Sequelize.Op;
+    Score.findAll({
+      where: {
+        HospitalId: hospitalId,
+        createdAt: {
+          [Op.lt]: new Date(),
+          // last 15 min records
+          [Op.gt]: new Date(new Date() - 15 * 60 * 1000),
         },
-      }).then(scores => {
-        const lastScores = scores.map(score => score.toJSON());
-        const sumScores = lastScores.reduce(
+      },
+    })
+      .then(scores => {
+        const readableScores = scores.map(score => score.toJSON());
+        const sumScores = readableScores.reduce(
           (acc, score) => acc + score.rate,
           0
         );
-        const avgScore = Math.round(sumScores / lastScores.length);
+        const avgScore = Math.round(sumScores / readableScores.length);
 
         Hospital.findByPk(hospitalId).then(hospital => {
           try {
@@ -43,18 +44,18 @@ exports.sendHospitalScore = function(req, res) {
             console.error(
               `Error ${e} happened after trying to update hospital status in Hospitals`
             );
-            throw e;
           }
         });
-      }).catch (e) {
-      res.status(500);
-      const message = e.message;
-      console.error(
-        `Error ${e} happened when trying to send a new hospital status.`
-      );
-      res.text(
-        `Error happened when trying to send a new hospital status; \n  ${message}`
-      );
-    }
+      })
+      .catch(e => {
+        res.status(500);
+        const message = e.message;
+        console.error(
+          `Error ${e} happened when trying to send a new hospital status.`
+        );
+        res.text(
+          `Error happened when trying to send a new hospital status; \n  ${message}`
+        );
+      });
   });
 };
