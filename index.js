47ae914c19e9753c530a023c70917656fb6e7698
Ivan Ortega <ivanortegaalba@gmail.com>
2020-04-12 17:16:18 +0200

    feat: add monitorization using New Relic Node Agent

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'production') {
     path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env',
   });
 }
+require('newrelic');
 const express = require('express');
 const app = express();
 const db = require('./db');
