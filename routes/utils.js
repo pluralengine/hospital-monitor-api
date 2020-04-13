c7581c92f8f4acee17e7f54cf88b61c5bc747e69
Ivan Ortega <ivanortegaalba@gmail.com>
2020-04-13 10:16:31 +0200

    feat: add hospital endpoint

--- routes/utils.js
+++ routes/utils.js
@@ -6,10 +6,10 @@ exports.authenticateJWT = (req, res, next) => {
   if (authHeader) {
     const token = authHeader.split(' ')[1];
 
-    jwt.verify(token, process.env.ACCESSTOKEN, (err, user) => {
-      if (err) {
+    jwt.verify(token, process.env.ACCESSTOKEN, (error, user) => {
+      if (error) {
         res.status(403);
-        return res.send({ error: 'Wrong authentication credentials' });
+        return res.send({ error: `${error}` });
       }
       req.user = user;
       next();
